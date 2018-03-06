9123a855f91582286f9d8c205228640c97d43c11
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-06 09:26:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
-var api = "http://apihost.com:port";
+var api = "http://ca.npool.pw:10080";
 
-var poolHost = "apihost.com";
+var poolHost = "ca.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/coin";
+var blockExplorer = "http://blockexplorer.pw/plura";
 
 var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "https://explorer.pluracoin.org/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "https://explorer.pluracoin.org/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"]
+        ["npool.pw/plura", "http://ca.npool.pw:10080"],
+        ["plurapool.com", "https://api.plurapool.com"]
 ];
